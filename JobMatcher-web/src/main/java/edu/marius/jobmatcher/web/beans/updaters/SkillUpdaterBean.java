ae8905f3c5265746448726eca95191e4899ffc1c
mcraciunescu <marius.sik93@yahoo.com>
2018-05-20 11:44:34 +0300

    add skill function
    add skill function

---
+++ JobMatcher-web/src/main/java/edu/marius/jobmatcher/web/beans/updaters/SkillUpdaterBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.marius.jobmatcher.web.beans.updaters;
+
+import edu.marius.jobmatcher.web.util.JsfUtils;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Marius
+ */
+@Named("skillUpdaterBean")
+@RequestScoped
+public class SkillUpdaterBean {
+
+    private String skill;
+
+    public void removeSkill(String skill) {
+        JsfUtils.getCv().ifPresent(cv -> cv.getSkills().getSkill().remove(skill));
+    }
+
+    public void addSkill() {
+        JsfUtils.getCv().ifPresent(cv -> cv.getSkills().getSkill().add(skill));
+        skill = null;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+}
